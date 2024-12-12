d9a8ed865877dbd75f80f9c0a2896c82834b145b
joecooney-UC <joecooney@uchicago.edu>
2024-12-12 14:57:17 -0600

    make B earlier

--- test_RHF_DIIS.py
+++ test_RHF_DIIS.py
@@ -207,7 +207,7 @@ def scf_procedure(mol, ethresh=1e-7, dmthresh=1e-7, maxiter=5):
             converge = True
             break
 
-        if i > 3:
+        if i > 0:
             # Build the B matrix
             B_dim = len(fock_List) + 1
             B = np.empty((B_dim, B_dim))
